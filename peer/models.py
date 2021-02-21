63730fca455132affb03349ef4230fd9124df7d9
Kei Sugano <kei.sugano@h-t.co.jp>
2021-02-21 19:15:31 +0900

    templates added etc

--- peer/models.py
+++ peer/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    firstName    = models.CharField(max_length=100)
-    lastName     = models.CharField(max_length=100)
+    pass
 
 class Credential(models.Model):
     user     = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,6 +10,8 @@ class Credential(models.Model):
     password = models.CharField(max_length=50)   # there should be minimum length
 
 class Profile(models.Model):
+    firstName    = models.CharField(max_length=100)
+    lastName     = models.CharField(max_length=100)
     user         = models.ForeignKey(User, on_delete=models.CASCADE)
     introduction = models.CharField(max_length=200)
     photo        = models.CharField(max_length=100) # physical path or S3
@@ -26,7 +27,7 @@ class Skill(models.Model):
     years   = models.IntegerField(default=0, max_length=50)
 
 class Replutation(models.Model):
-    name = models.CharField(max_length=50)
+    pass
 
 class Score(models.Model):
     reputation = models.ForeignKey(Replutation, on_delete=models.CASCADE)
--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,1 +1,6 @@
-python3 manage.py makemigrations peer
+if [[ $1 == '-c' ]]; then
+    rm -rf peer/migrations/*
+    python3 manage.py makemigrations peer
+else
+    python3 manage.py makemigrations peer
+fi
